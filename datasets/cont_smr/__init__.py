61bc48dbc7322cfeee0397ac3158f4cc9e3c641e
mralioo <ali.alouane@outlook.de>
2022-07-11 19:56:49 +0200

    ADDED: saving  of output for huge data

--- datasets/cont_smr/__init__.py
+++ datasets/cont_smr/__init__.py
@@ -1,55 +1,167 @@
 import os
+import pickle
 from collections import defaultdict
 from pathlib import Path
 
 import numpy as np
+import scipy.io
 from scipy.io import loadmat
 
 from lightning_torch.utils.file_mgmt import get_dir_by_indicator
 
-def load_subjects_by_filter(data_path, filter_key, outdir=None):
-    # DATA_PATH = Path(data_path)
-    # mat_files = []
-    # pattern = "*.mat"
-    # for file in DATA_PATH.glob(pattern):
-    #     mat_files.append(file)
-    #
-    # group_files = {}
-    # for f in mat_files:
-    #     res = f.stem.split("_", 1)
-    #     if res[0] in group_files:
-    #         group_files[res[0]][res[1]] = f
-    #     else:
-    #         group_files[res[0]] = {}
-    #         group_files[res[0]][res[1]] = f
-    # if filter_key == "gender":
-    pass
 
-
-def load_all_mat_sessions(data_path, outdir=None):
+def list_all_files(data_path, pattern="*.mat"):
     DATA_PATH = Path(data_path)
     mat_files = []
-    pattern = "*.mat"
+
     for file in DATA_PATH.glob(pattern):
         mat_files.append(file)
 
     group_files = {}
     for f in mat_files:
         res = f.stem.split("_", 1)
-        if res[0] in group_files:
-            group_files[res[0]][res[1]] = f
+        if len(res) > 1:
+            if res[0] in group_files:
+                group_files[res[0]][res[1]] = f
+            else:
+                group_files[res[0]] = {}
+                group_files[res[0]][res[1]] = f
         else:
-            group_files[res[0]] = {}
-            group_files[res[0]][res[1]] = f
+            group_files[res[0]] = f
+
+    return group_files
+
+
+def load_data_from_mat(mat_file, key):
+    mdata = loadmat(mat_file, mat_dtype=True)["BCI"]
+
+    # data for all 450 trials:
+    if key == "data":
+        data = mdata["data"][0][0][0]
+        timepoints = mdata["time"][0][0][0]  # time
+        fs = mdata["SRATE"][0][0][0][0]  # srate
+        return data, timepoints, fs
+
+    # chaninfo = defaultdict(list)
+    # chaninfo_keys = mdata["chaninfo"][0][0][0].dtype.names
+    # Extracting channel names and mnt info, excluding the reference electrode (REF)
+    elif key == "clab":
+        chan_inf = mdata["chaninfo"][0][0][0]["electrodes"][0][0]
+        clab = []
+        mnt = []
+        for element in chan_inf[:-1]:
+            clab.append(str(element[0][0]))
+            mnt.append([element[1][0][0], element[2][0][0], element[3][0][0]])
+        mnt = np.array(mnt)
+        clab = np.array(clab)
+        return mnt, clab
+
+    elif key == "trial_info":
+        trial_info = defaultdict(list)
+        trial_info_keys = mdata["TrialData"][0][0][0].dtype.names
+        for trial in mdata["TrialData"][0][0][0]:
+            for i, key in enumerate(trial_info_keys):
+                trial_info[key].append(trial[i][0][0])
+        return trial_info
+
+    elif key == "metadata":
+        # trial artifact from the original data set, if bandpass filtered data from any electrode crosses threshold of +-100 μV = True, else False
+        metadata = dict()
+        metadata_keys = mdata["metadata"][0][0][0].dtype.names
+
+        for s in mdata["metadata"][0][0][0]:
+            for i, key in enumerate(metadata_keys):
+                metadata[key] = s[i][0][0]
+        return metadata
+    else:
+        # data for all 450 trials:
+        data = mdata["data"][0][0][0]
+
+        # time:
+        timepoints = mdata["time"][0][0][0]
+
+        # srate
+        fs = mdata["SRATE"][0][0][0][0]
+
+        # chaninfo = defaultdict(list)
+        # chaninfo_keys = mdata["chaninfo"][0][0][0].dtype.names
+        # Extracting channel names and mnt info, excluding the reference electrode (REF)
+        chan_inf = mdata["chaninfo"][0][0][0]["electrodes"][0][0]
+        clab = []
+        mnt = []
+        for element in chan_inf[:-1]:
+            clab.append(str(element[0][0]))
+            mnt.append([element[1][0][0], element[2][0][0], element[3][0][0]])
+        mnt = np.array(mnt)
+        clab = np.array(clab)
+
+        trial_info = defaultdict(list)
+
+        trial_info_keys = mdata["TrialData"][0][0][0].dtype.names
+        for trial in mdata["TrialData"][0][0][0]:
+            for i, key in enumerate(trial_info_keys):
+                trial_info[key].append(trial[i][0][0])
+
+        metadata = dict()
+
+        metadata_keys = mdata["metadata"][0][0][0].dtype.names
+
+        for s in mdata["metadata"][0][0][0]:
+            for i, key in enumerate(metadata_keys):
+                metadata[key] = s[i][0][0]
+        # trial artifact from the original data set, if bandpass filtered data from any electrode crosses threshold of +-100 μV = True, else False
+        return data, timepoints, fs, clab, mnt, trial_info, metadata
+
+
+def load_all_eeg_data(data_path, outdir=None):
+    group_files = list_all_files(data_path)
+    res = {}
+    for subject, sessions in group_files.items():
+        if subject not in res:
+            res[subject] = {}
+            print(f"Subject {subject} : \n")
+            for i, (session_id, session_path) in enumerate(sessions.items()):
+                if session_id not in res[subject]:
+                    res[subject][session_id] = {}
+                    try:
+                        data, timepoints, fs = load_data_from_mat(session_path, key="data")
+                    except:
+                        res[subject][session_id]["loaded"] = "FAILED"
+
+                    print(
+                        f"Loading of {session_id} from {str(i + 1)}/{len(sessions.keys())} sessions",
+                        end="\n")
+
+                    res[subject][session_id]["data"] = data
+
+                    if outdir is not None:
+                        if not os.path.exists(outdir):
+                            os.makedirs(outdir)
+                        outfile_name = os.path.join(outdir, f"{subject}_{session_id}.npz")
+                        np.savez_compressed(outfile_name, data=data)
+
+    return res
+
+
+def load_all_mat_sessions(data_path, outdir=None):
+    group_files = list_all_files(data_path)
 
     res = {}
     for subject, sessions in group_files.items():
         if subject not in res:
             res[subject] = {}
-            for session_id, session_path in sessions.items():
+            for i, (session_id, session_path) in enumerate(sessions.items()):
                 if session_id not in res[subject]:
                     res[subject][session_id] = {}
-                    data, timepoints, fs, clab, mnt, trial_info, metadata = load_data_from_mat(session_path)
+                    try:
+                        data, timepoints, fs, clab, mnt, trial_info, metadata = load_data_from_mat(session_path, key="")
+                    except:
+                        res[subject][session_id]["loaded"] = "FAILED"
+
+                    print(
+                        f"Subject {subject} : Loading {session_id} from {str(i + 1)}/{len(sessions.keys())} sessions ",
+                        end="\r")
+
                     res[subject][session_id]["data"] = data
                     res[subject][session_id]["timepoints"] = timepoints
                     res[subject][session_id]["fs"] = fs
@@ -57,54 +169,16 @@ def load_all_mat_sessions(data_path, outdir=None):
                     res[subject][session_id]["mnt"] = mnt
                     res[subject][session_id]["trial_info"] = trial_info
                     res[subject][session_id]["metadata"] = metadata
-            if outdir is not None:
-                outfile_name = os.path.join(outdir, f"{subject}_{session_id}")
-                np.savez(outfile_name, X=data, timepoints=timepoints, fs=fs, clab=clab, mnt=mnt, trial_info=trial_info,
-                         metadata=metadata)
+
+                    if outdir is not None:
+                        if not os.path.exists(outdir):
+                            os.makedirs(outdir)
+                        outfile_name = os.path.join(outdir, f"{subject}_{session_id}_all.mat")
+                        scipy.io.savemat(outfile_name,
+                                         {"data": data, "timepoints": timepoints, "fs": fs, "lab": clab, "mnt": mnt,
+                                          "trial_info": trial_info, "metadata": metadata})
 
     return res
-
-
-def load_data_from_mat(mat_file):
-    mdata = loadmat(mat_file, mat_dtype=True)["BCI"]
-
-    # data for all 450 trials:
-    data = mdata["data"][0][0][0]
-
-    # time:
-    timepoints = mdata["time"][0][0][0]
-
-    # srate
-    fs = mdata["SRATE"][0][0][0][0]
-
-    # chaninfo = defaultdict(list)
-    # chaninfo_keys = mdata["chaninfo"][0][0][0].dtype.names
-    # Extracting channel names and mnt info, excluding the reference electrode (REF)
-    chan_inf = mdata["chaninfo"][0][0][0]["electrodes"][0][0]
-    clab = []
-    mnt = []
-    for element in chan_inf[:-1]:
-        clab.append(str(element[0][0]))
-        mnt.append([element[1][0][0], element[2][0][0], element[3][0][0]])
-    mnt = np.array(mnt)
-    clab = np.array(clab)
-
-    trial_info = defaultdict(list)
-
-    trial_info_keys = mdata["TrialData"][0][0][0].dtype.names
-    for trial in mdata["TrialData"][0][0][0]:
-        for i, key in enumerate(trial_info_keys):
-            trial_info[key].append(trial[i][0][0])
-
-    metadata = dict()
-
-    metadata_keys = mdata["metadata"][0][0][0].dtype.names
-
-    for s in mdata["metadata"][0][0][0]:
-        for i, key in enumerate(metadata_keys):
-            metadata[key] = s[i][0][0]
-    # trial artifact from the original data set, if bandpass filtered data from any electrode crosses threshold of +-100 μV = True, else False
-    return data, timepoints, fs, clab, mnt, trial_info, metadata
 
 
 def load_single_mat_session(subjectno, sessionno, data_path, outdir=None):
@@ -113,41 +187,77 @@ def load_single_mat_session(subjectno, sessionno, data_path, outdir=None):
         DATA_PATH = Path(root_dir).parent / "data" / "raw"  # the data folder has to be in the parent folder!
     else:
         DATA_PATH = Path(data_path)
-    group_files = {}
 
-    mat_files = []
-    pattern = "*.mat"
-    for file in DATA_PATH.glob(pattern):
-        mat_files.append(file)
-    for f in mat_files:
-        res = f.stem.split("_", 1)
-        if res[0] in group_files:
-            group_files[res[0]][res[1]] = f
-        else:
-            group_files[res[0]] = {}
-            group_files[res[0]][res[1]] = f
+    group_files = list_all_files(DATA_PATH)
 
     # Select the subject person and the session number to load the data
     subject = "S" + str(subjectno)
-    session = "Session_" + str(sessionno)
-    file_path = os.path.join(DATA_PATH, group_files[subject][session])
+    session_id = "Session_" + str(sessionno)
+    file_path = os.path.join(DATA_PATH, group_files[subject][session_id])
     if os.path.exists(file_path):
-        mdata = loadmat(file_path, mat_dtype=True)["BCI"]
+        data, timepoints, fs, clab, mnt, trial_info, metadata = load_data_from_mat(file_path)
     else:
         raise FileNotFoundError(f"{file_path} file does not exist !")
 
-    data, timepoints, fs, clab, mnt, trial_info, metadata = load_data_from_mat(file_path)
-
     if outdir is not None:
-        outfile_name = os.path.join(outdir, f"{subject}_{session}")
-        np.savez(outfile_name, X=data, timepoints=timepoints, fs=fs, clab=clab, mnt=mnt, trial_info=trial_info,
-                 metadata=metadata)
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        outfile_name = os.path.join(outdir, f"{subject}_{session_id}_all.mat")
+        scipy.io.savemat(outfile_name,
+                         {"data": data, "timepoints": timepoints, "fs": fs, "lab": clab, "mnt": mnt,
+                          "trial_info": trial_info, "metadata": metadata})
     else:
         return data, timepoints, fs, clab, mnt, trial_info, metadata
 
 
-def trial_ids_key(data, keys):
-    pass
+def load_subjects_info(data_path, outdir=None):
+    group_files = list_all_files(data_path)
+    subjects_stats = {}
+    for subject, sessions in group_files.items():
+        if subject not in subjects_stats:
+            subjects_stats[subject] = {}
+            for session_path in sessions.values():
+                try:
+                    metadata = load_data_from_mat(session_path, key="metadata")
+                    if len(metadata) != 0:
+                        break
+                except:
+                    pass
+            subjects_stats[subject]["metadata"] = metadata
+        if outdir is not None:
+            if not os.path.exists(outdir):
+                os.makedirs(outdir)
+            outfile_name = os.path.join(outdir, f"{subject}.pkl")
+            with open(outfile_name, "wb") as f:
+                pickle.dump(subjects_stats, f, pickle.HIGHEST_PROTOCOL)
+    return subjects_stats
+
+
+def load_subjects_trials_stats(data_path, outdir=None):
+    group_files = list_all_files(data_path)
+    res = {}
+    for subject, sessions in group_files.items():
+        if subject not in res:
+            res[subject] = {}
+            for i, (session_id, session_path) in enumerate(sessions.items()):
+                if session_id not in res[subject]:
+                    res[subject][session_id] = {}
+                    try:
+                        trial_info = load_data_from_mat(session_path, key="trial_info")
+                    except:
+                        res[subject][session_id]["loaded"] = "FAILED"
+
+                    res[subject][session_id]["result"] = np.array(trial_info["result"])
+                    res[subject][session_id]["forcedresult"] = np.array(trial_info["forcedresult"])
+                    res[subject][session_id]["triallength"] = np.array(trial_info["triallength"])
+
+                if outdir is not None:
+                    if not os.path.exists(outdir):
+                        os.makedirs(outdir)
+                    outfile_name = os.path.join(outdir, f"{subject}_{session_id}.pkl")
+                    with open(outfile_name, "wb") as f:
+                        pickle.dump(res, f, pickle.HIGHEST_PROTOCOL)
+    return res
 
 
 if __name__ == "__main__":
--- datasets/preprocess_utils.py
+++ datasets/preprocess_utils.py
@@ -1,83 +1,76 @@
-import os
-import random
-import sys
-from datasets.preprocess_utils import *
-from datimport os
-import random
-import sys
-from datasets.preprocess_utils import *
-from datasets.cont_smr import *
-from plotly.offline import init_notebook_mode
+import pickle
 
-module_path = os.path.abspath(os.path.join('..'))
-if module_path not in sys.path:
-    sys.path.append(module_path)
+import numpy as np
 
-init_notebook_mode(connected=True)  ## plotly init
-seed = 123
-random.seed = seed
-
-from datasets.cont_smr import *
-from bokeh.io import output_notebook
-
-output_notebook()asets.cont_smr import *
-from plotly.offline import init_notebook_mode
-
-module_path = os.path.abspath(os.path.join('..'))
-if module_path not in sys.path:
-    sys.path.append(module_path)
-
-init_notebook_mode(connected=True)  ## plotly init
-seed = 123
-random.seed = seed
-
-from datasets.cont_smr import *
-from bokeh.io import output_notebook
-
-output_notebook()
+from datasets.cont_smr import list_all_files
 
 
-def sort_by_gender(subjects_dict):
-    res_dict = {"M": [], "F": []}
-    for subject, sessions in subjects_dict.items():
-        sessions_id = list(sessions.keys())
-        gender = sessions[sessions_id[0]]["metadata"]["gender"]
-        res_dict[gender].append(subjects_dict[subject])
+def load_dict_pkl(path_name):
+    res_dict = {}
+    group_files = list_all_files(path_name, pattern="*.pkl")
+    for _, subject_value in group_files.items():
+        if isinstance(subject_value, dict):
+            for _, session_path in subject_value.items():
+                with open(session_path, "rb") as f:
+                    res_dict.update(pickle.load(f))
+        else:
+            with open(subject_value, "rb") as f:
+                res_dict.update(pickle.load(f))
     return res_dict
 
 
-def sort_by_handedness(subjects_dict):
-    res_dict = {"R": [], "L": []}
-    for subject, sessions in subjects_dict.items():
-        sessions_id = list(sessions.keys())
-        handedness = sessions[sessions_id[0]]["metadata"]["handedness"]
-        res_dict[handedness].append(subjects_dict[subject])
-    return res_dict
-
-
-def get_trial_stats(session_dict):
+def get_trial_stats(trial_info_dict):
     trial_stat_dict = {}
-    true_trials_idx = np.where(np.array(session_dict["trial_info"]["result"]) == True)[0]
-    false_trials_idx = np.where(np.array(session_dict["trial_info"]["result"]) == False)[0]
-    error_trials_idx = np.where(np.isnan(np.array(session_dict["trial_info"]["result"])))[0]
-    trial_stat_dict["true"] = len(true_trials_idx)
-    trial_stat_dict["false"] = len(false_trials_idx)
-    trial_stat_dict["error"] = len(error_trials_idx)
-    trial_stat_dict["mean_triallength"] = np.mean(np.array(session_dict["trial_info"]["triallength"]))
-    trial_stat_dict["std_triallength"] = np.std(np.array(session_dict["trial_info"]["triallength"]))
+    for subject, sessions in trial_info_dict.items():
+        trial_stat_dict[subject] = {}
+        for session_id, values in sessions.items():
+            trial_stat_dict[subject][session_id] = {}
+
+            true_trials_idx = np.where(np.array(values["result"]) == True)[0]
+            false_trials_idx = np.where(np.array(values["result"]) == False)[0]
+            error_trials_idx = np.where(np.isnan(np.array(values["result"])))[0]
+
+            trial_stat_dict[subject][session_id]["true"] = len(true_trials_idx)
+            trial_stat_dict[subject][session_id]["false"] = len(false_trials_idx)
+            trial_stat_dict[subject][session_id]["error"] = len(error_trials_idx)
+            trial_stat_dict[subject][session_id]["mean_triallength"] = np.mean(np.array(values["triallength"]))
+            trial_stat_dict[subject][session_id]["std_triallength"] = np.std(np.array(values["triallength"]))
+
     return trial_stat_dict
 
 
-def get_subjects_stats(subjects_dict):
-    subjects_stats = {}
-    for subject, sessions in subjects_dict.items():
-        sessions_id = list(sessions.keys())
-        subjects_stats[subject] = {}
-        subjects_stats[subject]["gender"] = sessions[sessions_id[0]]["metadata"]["gender"]
-        subjects_stats[subject]["age"] = sessions[sessions_id[0]]["metadata"]["age"]
-        subjects_stats[subject]["handedness"] = sessions[sessions_id[0]]["metadata"]["handedness"]
-        subjects_stats[subject]["handsport"] = sessions[sessions_id[0]]["metadata"]["handsport"]
-        subjects_stats[subject]["instrument"] = sessions[sessions_id[0]]["metadata"]["instrument"]
-        subjects_stats[subject]["meditationpractice"] = sessions[sessions_id[0]]["metadata"]["meditationpractice"]
-        subjects_stats[subject]["MBSRsubject"] = sessions[sessions_id[0]]["metadata"]["MBSRsubject"]
-    return subjects_stats
+def get_subjects_stats(subjects_info_dict, filters):
+    if filters == "gender":
+        res_dict = {"M": 0, "F": 0}
+        for subject, metadata in subjects_info_dict.items():
+            res_dict[metadata["metadata"]["gender"]] += 1
+    elif filters == "handedness":
+        res_dict = {"R": 0, "L": 0}
+        for subject, metadata in subjects_info_dict.items():
+            res_dict[metadata["metadata"]["handedness"]] += 1
+    elif filters == "handsport":
+        res_dict = {"Y": 0, "N": 0}
+        for subject, metadata in subjects_info_dict.items():
+            res_dict[metadata["metadata"]["handsport"]] += 1
+    elif filters == "MBSRsubject":
+        res_dict = {True: 0, False: 0}
+        for subject, metadata in subjects_info_dict.items():
+            res_dict[metadata["metadata"]["MBSRsubject"]] += 1
+    elif filters == "age":
+        res_dict = {"Total": [], "F": [], "M": []}
+        for subject, metadata in subjects_info_dict.items():
+            res_dict["Total"].append(metadata["metadata"]["age"])
+            if metadata["metadata"]["gender"] == "F":
+                res_dict["F"].append(metadata["metadata"]["age"])
+            else:
+                res_dict["M"].append(metadata["metadata"]["age"])
+    elif filters == "meditationpractice":
+        res_dict = {"Total": [], "F": [], "M": []}
+        for subject, metadata in subjects_info_dict.items():
+            res_dict["Total"].append(metadata["metadata"]["meditationpractice"])
+            if metadata["metadata"]["gender"] == "F":
+                res_dict["F"].append(metadata["metadata"]["meditationpractice"])
+            else:
+                res_dict["M"].append(metadata["metadata"]["meditationpractice"])
+
+    return res_dict
