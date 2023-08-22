c02463d386a2330b49971be92d6bc192d7a45369
mralioo <ali.alouane@outlook.de>
2023-08-22 23:30:55 +0200

    ADDED: hydra script test run

---
+++ ida_cluster_setup/first_run_test.cmd
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=cpu_job
+#SBATCH --partition=cpu-2d
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --output=logs/job-%j.out
+
+
+
+apptainer run --nv bbcpy_lightning.sif python src/baseline_train.py experiment=baseline_riemann
