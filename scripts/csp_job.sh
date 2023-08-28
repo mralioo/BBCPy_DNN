e0e7482a610b74fac829378962385cb09c1a3a19
ali_alouane <ali_alouane@cluster.ml.tu-berlin.de>
2023-08-28 18:17:31 +0200

    FIXED: config exp override params

--- scripts/csp_job.sh
+++ scripts/csp_job.sh
@@ -6,12 +6,13 @@
 #SBATCH --output=logs/job-%j.out
 #SBATCH --output=../jobs_outputs/csp-S9/%x_%j.o
 #SBATCH --error=../jobs_outputs/csp-S9/%x_%j.e
+#SBATCH --mail-user $mr.ali.alouane@gmail.com
 
 echo "I am a job with ID $SLURM_JOB_ID"
 echo "current working directory is $(pwd)"
 
 # 1. copy the squashed dataset to the nodes /tmp
-cp ./squashfs_smr_data/S9_1234567891011.sqfs /tmp/
+cp ./../squashfs_smr_data/S9_1234567891011.sqfs /tmp/
 
 # 3. bind the squashed dataset to your apptainer environment and run your script with apptainer
-apptainer run -B /tmp/S9_1234567891011.sqfs:/input-data:image-src=/ ./env_images/bbcpy_lightning_v3.sif python ./bbcpy_autoML/src/baseline_train.py +experiment=0_csp
+apptainer run -B /tmp/S9_1234567891011.sqfs:/input-data:image-src=/ ./../env_images/bbcpy_lightning_v3.sif python ./src/baseline_train.py +experiment=0_csp
