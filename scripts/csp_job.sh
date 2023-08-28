555b46a46cabbcf90a8d9c82e21641b68bbaa439
mralioo <ali.alouane@outlook.de>
2023-08-28 16:15:52 +0200

    ADDED: hydra scripts jobs baseline models

--- scripts/csp_job.sh
+++ scripts/csp_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=riemann-S9
+#SBATCH --job-name=csp-S9
 #SBATCH --partition=cpu-2h
 #SBATCH --gpus-per-node=0
 #SBATCH --ntasks-per-node=2
 #SBATCH --output=logs/job-%j.out
-#SBATCH --output=../jobs_outputs/riemann-S9/%x_%j.o
-#SBATCH --error=../jobs_outputs/riemann-S9/%x_%j.e
+#SBATCH --output=../jobs_outputs/csp-S9/%x_%j.o
+#SBATCH --error=../jobs_outputs/csp-S9/%x_%j.e
 
 echo "I am a job with ID $SLURM_JOB_ID"
 echo "current working directory is $(pwd)"
@@ -14,4 +14,4 @@ echo "current working directory is $(pwd)"
 cp ./squashfs_smr_data/S9_1234567891011.sqfs /tmp/
 
 # 3. bind the squashed dataset to your apptainer environment and run your script with apptainer
-apptainer run -B /tmp/S9_1234567891011.sqfs:/input-data:image-src=/ ./env_images/bbcpy_lightning_v3.sif python ./bbcpy_autoML/src/baseline_train.py +experiment=0_riemann
+apptainer run -B /tmp/S9_1234567891011.sqfs:/input-data:image-src=/ ./env_images/bbcpy_lightning_v3.sif python ./bbcpy_autoML/src/baseline_train.py +experiment=0_csp
