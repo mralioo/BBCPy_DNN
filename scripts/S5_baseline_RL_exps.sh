0e56473b6f40f49013256973f0839786927d148e
mralioo <ali.alouane@outlook.de>
2023-09-01 04:46:48 +0200

    ADDED: scripts for baseline S1-S10

---
+++ scripts/S5_baseline_RL_exps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name=S5-baseline
+#SBATCH --partition=cpu-2h
+#SBATCH --gpus-per-node=0
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=16
+#SBATCH --output=../jobs_outputs/S5/%x_%j.o
+#SBATCH --error=../jobs_outputs/S5/%x_%j.e
+#SBATCH --mail-user=mr.ali.alouane@gmail.com
+
+echo "I am a job with ID $SLURM_JOB_ID"
+echo "current working directory is $(pwd)"
+
+# 1. copy the squashed dataset to the nodes /tmp
+cp ./../squashfs_smr_data/S5.sqfs /tmp/
+
+# Define experiments you want to run
+experiments=("0_csp" "0_mbcsp" "0_riemann" "0_riemann_tangent")
+
+# Loop over the experiments
+for experiment in "${experiments[@]}"; do
+
+    echo "Processing experiment $experiment"
+
+    # 3. bind the squashed dataset to your apptainer environment and run your script with apptainer
+    apptainer run -B /tmp/S5.sqfs:/input-data:image-src=/ ./../env_images/bbcpy_lightning_v3.sif python ./src/baseline_train.py +experiment=${experiment} +data.train_subjects_sessions_dict="{S5: "all"}" logger.mlflow.experiment_name="A-RL-S5-all"
+
+done
