8f2fa9618b0056265621fc8da8a9454c42242992
mralioo <ali.alouane@outlook.de>
2023-09-08 05:57:53 +0200

    minors

--- src/dnn_train.py
+++ src/dnn_train.py
@@ -126,10 +126,10 @@ def print_gpu_info():
 def main(cfg: DictConfig) -> Optional[float]:
     # apply extra utilities
     # (e.g. ask for tags if none are provided in cfg, print cfg tree, etc.)
-    utils.extras(cfg)
-
-
+    
     print_gpu_info()
+
+    utils.extras(cfg)
 
     # train the model
     metric_dict, _ = train(cfg)
