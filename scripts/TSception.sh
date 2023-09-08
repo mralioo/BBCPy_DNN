8c378e5ec400e3f351ae29073bcb0955e1aa9ee8
mralioo <ali.alouane@outlook.de>
2023-09-09 01:15:48 +0200

    FIXED: array job for eegnet and Tsception

---
+++ scripts/TSception.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=TSception-S1
+#SBATCH --partition=gpu-test
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --output=../jobs_outputs/TSception-S1/%x_%j.o
+#SBATCH --error=../jobs_outputs/TSception-S1/%x_%j.e
+
+echo "I am a job with ID $SLURM_JOB_ID"
+echo "current working directory is $(pwd)"
+
+# 1. copy the squashed dataset to the nodes /tmp
+cp ./../squashfs_smr_data/S1.sqfs /tmp/
+#export CUDA_VISIBLE_DEVICES=0
+# 3. bind the squashed dataset to your apptainer environment and run your script with apptainer
+apptainer run --nv -B /tmp/S1.sqfs:/input-data:image-src=/ ./../env_images/bbcpy_lightning_v5.sif python ./src/dnn_train.py experiment=TSCeption +data.subject_sessions_dict="{S1: "all"}"
---
+++ scripts/Tsception_array_1_20.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=S1-S20-TSCeption
+#SBATCH --partition=gpu-2h
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --output=../jobs_outputs/TSCeption-S%a/%x_%j.o
+#SBATCH --error=../jobs_outputs/TSCeption-S%a/%x_%j.e
+#SBATCH --mail-user=mr.ali.alouane@gmail.com
+#SBATCH --array=1-20
+
+echo "I am a job with ID $SLURM_JOB_ID for subject S$SLURM_ARRAY_TASK_ID"
+echo "current working directory is $(pwd)"
+
+SUBJECT="S$SLURM_ARRAY_TASK_ID"
+
+# 1. copy the squashed dataset to the nodes /tmp
+cp ./../squashfs_smr_data/${SUBJECT}.sqfs /tmp/
+
+echo "Processing data for subject $SUBJECT"
+
+# 3. bind the squashed dataset to your apptainer environment and run your script with apptainer
+apptainer run -B /tmp/${SUBJECT}.sqfs:/input-data:image-src=/ ./../env_images/bbcpy_lightning_v5.sif python ./src/dnn_train.py experiment=Tsception +data.subject_sessions_dict="{$SUBJECT: "all"}" logger.mlflow.run_name="${SUBJECT}-all-RL"
+
+
