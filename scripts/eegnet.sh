69bcc5eddbe7817fdc1293eb5cda05bb74818a29
mralioo <ali.alouane@outlook.de>
2023-09-08 03:12:10 +0200

    FIXED: configs for dnn torcheeg

--- scripts/eegnet.sh
+++ scripts/eegnet.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=eegnet-S1
-#SBATCH --partition=cpu-2h
-#SBATCH --gpus-per-node=0
-#SBATCH --ntasks-per-node=8
+#SBATCH --partition=gpu-2h
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=16
 #SBATCH --output=../jobs_outputs/eegnet-S1/%x_%j.o
 #SBATCH --error=../jobs_outputs/eegnet-S1/%x_%j.e
+#SBATCH --mail-user=mr.ali.alouane@gmail.com
 
 echo "I am a job with ID $SLURM_JOB_ID"
 echo "current working directory is $(pwd)"
@@ -13,4 +14,4 @@ echo "current working directory is $(pwd)"
 cp ./../squashfs_smr_data/S1.sqfs /tmp/
 
 # 3. bind the squashed dataset to your apptainer environment and run your script with apptainer
-apptainer run -B /tmp/S1.sqfs:/input-data:image-src=/ ./../env_images/bbcpy_lightning_v4.sif python ./src/dnn_train.py +experiment=eegnet.yaml
+apptainer run -B /tmp/S1.sqfs:/input-data:image-src=/ ./../env_images/bbcpy_lightning_v5.sif python ./src/dnn_train.py +experiment=eegnet.yaml +data.subject_sessions_dict="{"S1": "all"}" logger.mlflow.experiment_name="eegnet-S1-all"
