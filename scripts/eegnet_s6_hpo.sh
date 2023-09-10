dc1f4be7a8e63ac0c2eeefc6f7fcef1bdcd6ffdc
mralioo <ali.alouane@outlook.de>
2023-09-10 18:15:56 +0200

    ADDED: hpo eegnet

---
+++ scripts/eegnet_s6_hpo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=eegnet-hpo-S6
+#SBATCH --partition=gpu-5h
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --output=../jobs_outputs/eegnet-hpo-S6/%x_%j.o
+#SBATCH --error=../jobs_outputs/eegnet-hpo-S6/%x_%j.e
+
+echo "I am a job with ID $SLURM_JOB_ID"
+echo "current working directory is $(pwd)"
+
+# 1. copy the squashed dataset to the nodes /tmp
+cp ./../squashfs_smr_data/S6.sqfs /tmp/
+#export CUDA_VISIBLE_DEVICES=0
+# 3. bind the squashed dataset to your apptainer environment and run your script with apptainer
+apptainer run --nv -B /tmp/S6.sqfs:/input-data:image-src=/ ./../env_images/bbcpy_lightning_v5.sif python ./src/dnn_train.py experiment=eegnet +data.subject_sessions_dict="{S6: "all"}"
