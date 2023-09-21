be032a0872dfb4687252fb1a3ef7da2f402ca3ec
mralioo <ali.alouane@outlook.de>
2023-09-21 13:50:20 +0200

    REFAC:  old sh scripts

---
+++ scripts/debug_hpo_eegnet.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=debug-hpo-eegnet
+#SBATCH --partition=gpu-test
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --output=../jobs_outputs/debug-hpo-eegnet/%x_%j.o
+#SBATCH --error=../jobs_outputs/debug-hpo-eegnet/%x_%j.e
+
+echo "I am a job with ID $SLURM_JOB_ID"
+echo "current working directory is $(pwd)"
+
+# 1. copy the squashed dataset to the nodes /tmp
+cp ./../squashfs_smr_data/hpo_best_pvc.sqfs /tmp/
+#export CUDA_VISIBLE_DEVICES=0
+# 3. bind the squashed dataset to your apptainer environment and run your script with apptainer
+apptainer run --nv -B /tmp/hpo_best_pvc.sqfs:/input-data:image-src=/ ./../env_images/bbcpy_lightning_v5.sif python ./src/dnn_hpo_train.py experiment=eegnet +data.subject_sessions_dict="{S5: "all", S9:"all", S20: "all", S2:"all", S19:"all", S14:"all"}" hparams_search=eegnet_optuna.yaml logger.mlflow.run_name="eegnet-hpo-best-pvc"
