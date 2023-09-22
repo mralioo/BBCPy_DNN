5dd8ba2de221ad55d95b46727154fcf55a9f26d7
mralioo <ali.alouane@outlook.de>
2023-09-22 10:38:04 +0200

    FIXED: minors

--- scripts/hpo_eegnet.sh
+++ scripts/hpo_eegnet.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=eegnet-hpo-best-pvc
 #SBATCH --partition=gpu-5h
 #SBATCH --gpus-per-node=2
-#SBATCH --mem=5G
-#SBATCH --ntasks-per-node=2
+#SBATCH --mem=10G
+#SBATCH --ntasks-per-node=1
 #SBATCH --output=../jobs_outputs/eegnet-hpo-best-pvc/%x_%j.o
 #SBATCH --error=../jobs_outputs/eegnet-hpo-best-pvc/%x_%j.e
 
@@ -11,7 +11,7 @@ echo "I am a job with ID $SLURM_JOB_ID"
 echo "current working directory is $(pwd)"
 
 # 1. copy the squashed dataset to the nodes /tmp
-cp ./../squashfs_smr_data/S5.sqfs /tmp/
+cp ./../squashfs_smr_data/hpo_best_pvc_debug.sqfs /tmp/
 
 # 3. bind the squashed dataset to your apptainer environment and run your script with apptainer
-apptainer run --nv -B /tmp/S5.sqfs:/input-data:image-src=/ ./../env_images/bbcpy_lightning_v5.sif python ./src/dnn_hpo_train.py experiment=eegnet +data.subject_sessions_dict='{S5: "all"}' hparams_search=eegnet_optuna.yaml logger.mlflow.run_name="eegnet-hpo-best-pvc"
+apptainer run --nv -B /tmp/hpo_best_pvc_debug.sqfs:/input-data:image-src=/ ./../env_images/bbcpy_lightning_v5.sif python ./src/dnn_hpo_train.py experiment=eegnet +data.subject_sessions_dict='{S5: "all", S9:"all"}' hparams_search=eegnet_optuna.yaml logger.mlflow.run_name="eegnet-hpo-best-pvc"
