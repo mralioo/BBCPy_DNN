f58e9bb2c6c714e4dec89b8f4abe930aa0875593
mralioo <ali.alouane@outlook.de>
2023-09-22 20:17:29 +0200

    ADDED: increse ntasks and mem for job

--- scripts/debug_hpo_eegnet.sh
+++ scripts/debug_hpo_eegnet.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=debug-hpo-eegnet
 #SBATCH --partition=gpu-5h
 #SBATCH --gpus-per-node=1
-#SBATCH --mem=80G        # Some buffer above 30GB
-#SBATCH --ntasks-per-node=1   # One main task that runs the trial and manages CV
+#SBATCH --mem=100G        # Some buffer above 30GB
+#SBATCH --ntasks-per-node=5   # One main task that runs the trial and manages CV
 #SBATCH --cpus-per-task=5     # Assuming you want to run each CV fold in parallel
 #SBATCH --output=../jobs_outputs/debug-hpo-eegnet/%x_%j.o
 #SBATCH --error=../jobs_outputs/debug-hpo-eegnet/%x_%j.e
