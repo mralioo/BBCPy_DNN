603f93ce2f804f75aac8f9df067ec47fff1fc5cf
mralioo <ali.alouane@outlook.de>
2023-09-26 23:49:33 +0200

    FIXED: Tsception script

--- scripts/hpo_tsception.sh
+++ scripts/hpo_tsception.sh
@@ -9,12 +9,8 @@
 echo "I am a job with ID $SLURM_JOB_ID"
 echo "current working directory is $(pwd)"
 
-
-echo "I am a job with ID $SLURM_JOB_ID"
-echo "current working directory is $(pwd)"
-
 # 1. copy the squashed dataset to the nodes /tmp
 cp ./../squashfs_smr_data/hpo_best_pvc.sqfs /tmp/
 
 # 3. bind the squashed dataset to your apptainer environment and run your script with apptainer
-apptainer run --nv -B /tmp/hpo_best_pvc.sqfs.sqfs:/input-data:image-src=/ ./../env_images/bbcpy_en.sif python ./src/dnn_hpo_train.py experiment=Tsception_hpo +data.subject_sessions_dict='{S5: "all", S9:"all" ,S20:"all", S2:"all", S19:"all", S14:"all"}' hparams_search=tsception_optuna.yaml logger.mlflow.run_name="best-6-pvc"
+apptainer run --nv -B /tmp/hpo_best_pvc.sqfs:/input-data:image-src=/ ./../env_images/bbcpy_en.sif python ./src/dnn_hpo_train.py experiment=Tsception_hpo +data.subject_sessions_dict='{S5: "all", S9:"all" ,S20:"all", S2:"all", S19:"all", S14:"all"}' hparams_search=tsception_optuna.yaml logger.mlflow.run_name="best-6-pvc"
