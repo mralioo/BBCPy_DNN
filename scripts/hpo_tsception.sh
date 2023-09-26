eb88240c01b18335505cc61d1f7d26140de4eba4
ali_alouane <ali_alouane@cluster.ml.tu-berlin.de>
2023-09-27 00:14:23 +0200

    minors

--- scripts/hpo_tsception.sh
+++ scripts/hpo_tsception.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=TSCeption-hpo
+#SBATCH --job-name=TSCeption-debug-hpo
 #SBATCH --partition=gpu-2d
 #SBATCH --gpus-per-node=1
-#SBATCH --ntasks-per-node=2
-#SBATCH --output=../jobs_outputs/TSCeption-hpo/%x_%j.o
-#SBATCH --error=../jobs_outputs/TSCeption-hpo/%x_%j.e
+#SBATCH --mem=400G        # Some buffer above 30GB
+#SBATCH --ntasks-per-node=1   # One main task that runs the trial and manages CV
+#SBATCH --cpus-per-task=5 # Assuming you want to run each CV fold in parallel
+#SBATCH --output=../jobs_outputs/TSCeption-debug-hpo/%x_%j.o
+#SBATCH --error=../jobs_outputs/TSCeption-debug-hpo/%x_%j.e
 
 echo "I am a job with ID $SLURM_JOB_ID"
 echo "current working directory is $(pwd)"
