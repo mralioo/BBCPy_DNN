FIXED: softmax for all tasks

src/trainer/dnn_trainer.py
@@ -75,7 +75,7 @@ class DnnLitModule(LightningModule):
         logits = self.forward(x)
 
         if self.num_classes == 2:
-            probs = F.sigmoid(logits, dim=1)
+            probs = F.softmax(logits, dim=1)
         if self.num_classes > 2:
             probs = F.softmax(logits, dim=2)
 