7b53cf1ec0279643cce25a2eacfb0ae3b85ea9bf
mralioo <ali.alouane@outlook.de>
2023-09-30 01:48:16 +0200

    minors

--- src/trainer/dnn_trainer.py
+++ src/trainer/dnn_trainer.py
@@ -75,7 +75,7 @@ class DnnLitModule(LightningModule):
         logits = self.forward(x)
 
         if self.num_classes == 2:
-            probs = F.softmax(logits, dim=1)
+            probs = F.softmax(logits, dim=2)
         if self.num_classes > 2:
             probs = F.softmax(logits, dim=2)
 
