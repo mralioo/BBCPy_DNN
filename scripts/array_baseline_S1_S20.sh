42e47aa0a1f6906bc7b3a09e1e76614dd518dd60
mralioo <ali.alouane@outlook.de>
2023-10-01 00:49:37 +0200

    ADDED: baseline array 1-5 test

--- scripts/array_baseline_S1_S20.sh
+++ scripts/array_baseline_S1_S20.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=S1-S20-baseline
-#SBATCH --partition=cpu-2h
+#SBATCH --partition=cpu-2d
 #SBATCH --gpus-per-node=0
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=10
+#SBATCH --ntasks-per-node=3
 #SBATCH --output=../jobs_outputs/S%a/%x_%j.o
 #SBATCH --error=../jobs_outputs/S%a/%x_%j.e
 #SBATCH --mail-user=mr.ali.alouane@gmail.com
-#SBATCH --array=1-20
+#SBATCH --array=1-5
 
 echo "I am a job with ID $SLURM_JOB_ID for subject S$SLURM_ARRAY_TASK_ID"
 echo "current working directory is $(pwd)"
