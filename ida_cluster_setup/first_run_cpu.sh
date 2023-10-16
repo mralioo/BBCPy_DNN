0a6247af2841e6b15e9c3d93e2ae3e6ea4e01ef0
mralioo <ali.alouane@outlook.de>
2023-10-17 00:09:59 +0200

    minors

--- ida_cluster_setup/first_run_cpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=my_cpu_job
-#SBATCH --partition=cpu-test
-#SBATCH --gpus-per-node=0
-#SBATCH --ntasks-per-node=2
-#SBATCH --output=logs/job-%j.out
-
-apptainer run /opt/apps/pytorch-2.0.1-gpu.sif python example_script.py
--- ida_cluster_setup/first_run_gpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=my_gpu_job
-#SBATCH --partition=gpu-test
-#SBATCH --gpus-per-node=1
-#SBATCH --ntasks-per-node=2
-#SBATCH --output=logs/job-%j.out
-
-apptainer run /opt/apps/pytorch-2.0.1-gpu.sif python example_script.py
