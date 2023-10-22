c5066e08485319d0471de6dd0f011d053e57839c
mralioo <ali.alouane@outlook.de>
2023-10-22 19:27:48 +0200

    ADDED: csp without excllev

--- src/models/components/utils.py
+++ src/models/components/utils.py
@@ -1,9 +1,12 @@
-import torch
 import numpy as np
+import torch
+from numpy.lib.stride_tricks import as_strided as ast
+
 
 def get_trainable_parameter_num(model):
     total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     return total_params
+
 
 def L1Loss(model, Lambda):
     w = torch.cat([x.view(-1) for x in model.parameters()])
@@ -29,20 +32,22 @@ def generate_TS_channel_order(original_order: list):
         k = 0
         for s in [*channel[:]]:
             if s.isdigit():
-               k += 1
+                k += 1
         if k != 0:
-            chan_name.append(channel[:chan_name_len-k])
-            chan_num.append(int(channel[chan_name_len-k:]))
+            chan_name.append(channel[:chan_name_len - k])
+            chan_num.append(int(channel[chan_name_len - k:]))
             chan_final.append(channel)
     chan_pair = []
     for ch, id in enumerate(chan_num):
         if id % 2 == 0:
-            chan_pair.append(chan_name[ch] + str(id-1))
+            chan_pair.append(chan_name[ch] + str(id - 1))
         else:
-            chan_pair.append(chan_name[ch] + str(id+1))
+            chan_pair.append(chan_name[ch] + str(id + 1))
     chan_no_duplicate = []
     [chan_no_duplicate.extend([f, chan_pair[i]]) for i, f in enumerate(chan_final) if f not in chan_no_duplicate]
     return chan_no_duplicate[0::2] + chan_no_duplicate[1::2]
+
+
 def to_dense_prediction_model(model, axis=(2, 3)):
     """
     Transform a sequential model with strides to a model that outputs
@@ -107,3 +112,96 @@ def get_output_shape(model, in_chans, input_window_samples):
     return output_shape
 
 
+def norm_shape(shape):
+    '''
+    Normalize numpy array shapes so they're always expressed as a tuple,
+    even for one-dimensional shapes.
+    Parameters
+        shape - an int, or a tuple of ints
+    Returns
+        a shape tuple
+    '''
+    try:
+        i = int(shape)
+        return (i,)
+    except TypeError:
+        # shape was not a number
+        pass
+
+    try:
+        t = tuple(shape)
+        return t
+    except TypeError:
+        # shape was not iterable
+        pass
+
+    raise TypeError('shape must be an int, or a tuple of ints')
+
+
+def sliding_window(a, ws, ss=None, flatten=True):
+    '''
+    Return a sliding window over a in any number of dimensions
+    Parameters:
+        a  - an n-dimensional numpy array
+        ws - an int (a is 1D) or tuple (a is 2D or greater) representing the size
+             of each dimension of the window
+        ss - an int (a is 1D) or tuple (a is 2D or greater) representing the
+             amount to slide the window in each dimension. If not specified, it
+             defaults to ws.
+        flatten - if True, all slices are flattened, otherwise, there is an
+                  extra dimension for each dimension of the input.
+    Returns
+        an array containing each n-dimensional window from a
+    '''
+
+    if None is ss:
+        # ss was not provided. the windows will not overlap in any direction.
+        ss = ws
+    ws = norm_shape(ws)
+    ss = norm_shape(ss)
+
+    # convert ws, ss, and a.shape to numpy arrays so that we can do math in every
+    # dimension at once.
+    ws = np.array(ws)
+    ss = np.array(ss)
+    shape = np.array(a.shape)
+
+    # ensure that ws, ss, and a.shape all have the same number of dimensions
+    ls = [len(shape), len(ws), len(ss)]
+    if 1 != len(set(ls)):
+        raise ValueError( \
+            'a.shape, ws and ss must all have the same length. They were %s' % str(ls))
+
+    # ensure that ws is smaller than a in every dimension
+    if np.any(ws > shape):
+        raise ValueError( \
+            'ws cannot be larger than a in any dimension.\
+     a.shape was %s and ws was %s' % (str(a.shape), str(ws)))
+
+    # how many slices will there be in each dimension?
+    newshape = norm_shape(((shape - ws) // ss) + 1)
+    # the shape of the strided array will be the number of slices in each dimension
+    # plus the shape of the window (tuple addition)
+    newshape += norm_shape(ws)
+    # the strides tuple will be the array's strides multiplied by step size, plus
+    # the array's strides (tuple addition)
+    newstrides = norm_shape(np.array(a.strides) * ss) + a.strides
+    strided = ast(a, shape=newshape, strides=newstrides)
+    if not flatten:
+        return strided
+
+    # Collapse strided so that it has one more dimension than the window.  I.e.,
+    # the new array is a flat list of slices.
+    meat = len(ws) if ws.shape else 0
+    firstdim = (np.product(newshape[:-meat]),) if ws.shape else ()
+    dim = firstdim + (newshape[-meat:])
+    # remove any dimensions with size 1
+    # dim = filter(lambda i : i != 1,dim)
+    return strided.reshape(dim)
+
+
+if __name__ == "__main__":
+    a = np.arange(1000).reshape((10, 100))
+    ws = (10, 10)
+    ss = (10, 20)
+    t = sliding_window(a, ws, ss)
